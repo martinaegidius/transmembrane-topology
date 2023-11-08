d577115f5080cfc797c5e982330d14167d581ff0
martinaegidius <gygridd@gmail.com>
2023-11-08 14:44:44 +0100

    prepared simple dataloaders

---
+++ pdb_to_tensor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  8 11:51:40 2023
+
+@author: max
+"""
+
+###lets see if we can generate a graph from a pdb and save it as a tensor 
+from graphein.protein.tensor.io import protein_to_pyg
+
+
